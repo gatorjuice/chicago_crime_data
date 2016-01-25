6bf4707b516959e8423375bfc8d642652001e2f0
Jamie <gatorjuice@gmail.com>
2016-01-25 12:23:13 -0600

    completed entering attributes for creating new crime.

--- lib/chicago_crime_data.rb
+++ lib/chicago_crime_data.rb
@@ -2,17 +2,35 @@ require "chicago_crime_data/version"
 require "unirest"
 
 module ChicagoCrimeData
-  # Your code goes here...
+  
   class Crime
 
-    attr_reader :case_number, :beat, :x_coordinate, :block, :primary_type, :location_description, :date, :iucr, :domestic, :id, :ward, :arrest, :description, :y_coordinate, :updated_on, :fbi_code, :longitude, :year, :community_area, :latitude, :district
+    attr_accessor :case_number, :beat, :x_coordinate, :block, :primary_type, :location_description, :date, :iucr, :domestic, :id, :ward, :arrest, :description, :y_coordinate, :updated_on, :fbi_code, :longitude, :year, :community_area, :latitude, :district
 
     def initialize(input_options)
       @case_number = input_options["case_number"]
-      @primary_type = input_hash["primary_type"]
+      @primary_type = input_options["primary_type"]
       @beat = input_options["beat"]
       @year = input_options["year"]
       @description = input_options["description"]
+      @x_coordinate = input_options["x_coordinate"]
+      @y_coordinate = input_options["y_coordinate"]
+      @block = input_options["block"]
+      @primary_type = input_options["primary_type"]
+      @location_description = input_options["location_description"]
+      @date = input_options["date"]
+      @iucr = input_options["iucr"]
+      @domestic = input_options["domestic"]
+      @id = input_options["id"]
+      @ward = input_options["ward"]
+      @arrest = input_options["arrest"]
+      @updated_on = input_options["updated_on"]
+      @fbi_code = input_options["fbi_code"]
+      @longitude = input_options["longitude"]
+      @latitude = input_options["latitude"]
+      @year = input_options["year"]
+      @community_area = input_options["community_area"]
+      @district = input_options["district"]
     end
 
     def self.all
@@ -29,10 +47,6 @@ module ChicagoCrimeData
       string = input_hash[:string] || nil
       limit = input_hash[:limit] || nil
       offset = input_hash[:offset] || nil
-      p search
-      p string
-      p limit
-      p offset
       data = Unirest.get("https://data.cityofchicago.org/resource/ijzp-q8t2.json?#{search}=#{string}&$limit=#{limit}&$offset=#{offset}").body
       crimes = []
       data.each do |crime|
